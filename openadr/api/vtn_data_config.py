90fd665df1f035582a6bcfd9ad4675175dd3de88
Dimitris Agtzidis <agtzdimi@gmail.com>
2020-01-30 04:59:58 -0800

    Create function for DR Program Creation

--- openadr/api/vtn_data_config.py
+++ openadr/api/vtn_data_config.py
@@ -12,15 +12,23 @@
          "event_end": The time that the event will end in %Y-%m-%dT%H:%M:%S
          "event_notification": The time that the event will notify the VEN in %Y-%m-%dT%H:%M:%S
 
-   Example Run:
+         Example Run:
 
-curl -X POST -d '{"type":"event","drprogram":"Program1","event_start":"2020-01-30T02:15:42","event_end":"2020-01-30T02:16:52","event_notification":"2020-01-30T02:14"}' --header "Content-Type: application/json"  http://127.0.0.1:8000/vtn_data_config
+         curl -X POST -d '{"type":"event","drprogram":"Program1","event_start":"2020-01-30T02:15:42","event_end":"2020-01-30T02:16:52","event_notification":"2020-01-30T02:14"}' --header "Content-Type: application/json"  http://127.0.0.1:8000/vtn_data_config
 
+      In case of a "program" type:
+         "name": The DR Program name to be added
+         "sites": The VENs names in a string variable separated by comma e.g "site1,site2"
+         
+         Example Run:
+         
+         curl -X POST -d '{"type":"program","name":"Program2","sites":"Site1,ven02"}' --header "Content-Type: application/json"  http://127.0.0.1:8000/vtn_data_config
 """
 
 import django
 import os
 import json
+import sys
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.views.decorators.csrf import csrf_exempt
@@ -45,6 +53,12 @@ class VtnDataConfig(APIView):
       req = json.loads(req)
       if req['type'] == 'event':
          response = self.create_event(req['drprogram'], req['event_start'], req['event_end'], req['event_notification'])
+         if "ERROR" in response:
+            return Response(response, content_type='application/json', status=status.HTTP_400_BAD_REQUEST)
+         else:
+            return Response(response, content_type='application/json', status=status.HTTP_200_OK)
+      elif req['type'] == 'program':
+         response = self.create_program(req['name'], req['sites'])
          if "ERROR" in response:
             return Response(response, content_type='application/json', status=status.HTTP_400_BAD_REQUEST)
          else:
@@ -91,3 +105,20 @@ class VtnDataConfig(APIView):
               site=site)
       s.save()
       return '{"status": "OK"}'
+
+   def create_program(self,name,sites):
+      try:
+         siteList = sites.split(",")
+         finalSiteList = []
+         for site in siteList:
+            query = Site.objects.filter(Q(site_name=site))
+            finalSiteList.append(query[0])
+         programs = DRProgram.objects.all().order_by('id')
+         program = DRProgram(name=name)
+
+         program.save()
+         for site in finalSiteList:
+            program.sites.add(site)
+         return '{"status": "OK"}'
+      except Exception as e:
+         return '{"status": "ERROR: %s}' % e
